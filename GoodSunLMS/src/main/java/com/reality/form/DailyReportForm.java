d0957786a6ed820529cadcb152f074b2bb714db6
ou.ikutei <ou.ikutei@GS0112>
2023-06-07 15:14:44 +0900

    一旦dailyReport.html修復

--- GoodSunLMS/src/main/java/com/reality/form/DailyReportForm.java
+++ GoodSunLMS/src/main/java/com/reality/form/DailyReportForm.java
@@ -6,28 +6,38 @@ import java.util.Map;
 public class DailyReportForm {
 	
 	// やったこと
-	private ArrayList<DoneThings> doneThings;
+	private DoneThings doneThings;
 	// 所感
 	private String reflection;
+	
+	private ArrayList<DoneThings> doneThingsList = new ArrayList<>();
 	
 	public DailyReportForm() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public DailyReportForm(ArrayList<DoneThings> doneThings, String reflection) {
+	public DailyReportForm(ArrayList<DoneThings> doneThingsList, String reflection) {
 		super();
-		this.doneThings = doneThings;
+		this.doneThingsList = doneThingsList;
 		this.reflection = reflection;
 	}
 
-	public ArrayList<DoneThings> getDoneThings() {
+	public DoneThings getDoneThings() {
 		return doneThings;
 	}
-
-	public void setDoneThings(ArrayList<DoneThings> doneThings) {
+	public void setDoneThings(DoneThings doneThings) {
 		this.doneThings = doneThings;
+		setDoneThingsList(doneThings);
 	}
-
+	public ArrayList<DoneThings> getDoneThingsList() {
+		return doneThingsList;
+	}
+	public void setDoneThingsList(ArrayList<DoneThings> doneThingsList) {
+		this.doneThingsList = doneThingsList;
+	}
+	public void setDoneThingsList(DoneThings doneThings) {
+		this.doneThingsList.add(doneThings);
+	}
 	public String getReflection() {
 		return reflection;
 	}
--- GoodSunLMS/src/main/java/com/reality/util/Form2Excel.java
+++ GoodSunLMS/src/main/java/com/reality/util/Form2Excel.java
@@ -15,6 +15,9 @@ import javax.swing.filechooser.FileSystemView;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.hibernate.validator.internal.util.privilegedactions.NewInstance;
 import org.springframework.ui.Model;
 
@@ -36,8 +39,8 @@ public class Form2Excel {
 	 * 
 	 */
 	
-	HSSFWorkbook wb;
-	HSSFSheet ws;
+	XSSFWorkbook wb;
+	XSSFSheet ws;
 	
 	private DailyReportForm dailyReportForms = new DailyReportForm();
 	
@@ -51,10 +54,11 @@ public class Form2Excel {
 		// excel生成
 		
 		// template利用
-		String templatePath = "/Excel_template.xlsx";
+		String templatePath = "excel/Excel_template.xlsx";
+		System.out.println(this.getClass().getResourceAsStream(templatePath).toString());
 		InputStream tmpFile = this.getClass().getResourceAsStream(templatePath);
 
-		wb = new HSSFWorkbook(tmpFile);
+		wb = new XSSFWorkbook(tmpFile);
 		ws = wb.getSheetAt(0);
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
@@ -66,12 +70,12 @@ public class Form2Excel {
 		// 日付
 		this.setValue(2, 1, sdf.format(new Date()));
 		// 今やったこと
-		for (int i = 0; i < dailyReportForms.getDoneThings().size(); i++) {
+		for (int i = 0; i < dailyReportForms.getDoneThingsList().size(); i++) {
 			// 1行15字
-			this.setValue(row_pos, col_pos++, dailyReportForms.getDoneThings().get(i).getThings());
-			this.setValue(row_pos, col_pos++, dailyReportForms.getDoneThings().get(i).getCompleteness());
+			this.setValue(row_pos, col_pos++, dailyReportForms.getDoneThingsList().get(i).getThings());
+			this.setValue(row_pos, col_pos++, dailyReportForms.getDoneThingsList().get(i).getCompleteness());
 			// 1行20字
-			this.setValue(row_pos, col_pos++, dailyReportForms.getDoneThings().get(i).getImprovement());
+			this.setValue(row_pos, col_pos++, dailyReportForms.getDoneThingsList().get(i).getImprovement());
 			row_pos++;
 		}
 		// 所感 1行40字
@@ -80,8 +84,8 @@ public class Form2Excel {
 		// output
 		
  		File desktopDir = FileSystemView.getFileSystemView().getHomeDirectory();
-		String outputFilePath = desktopDir.getAbsolutePath();
-		String outputFileName = fileSdf.format(new Date()) +"_" + session.getAttribute("username") + ".xlsx";
+		String outputFilePath = desktopDir.getAbsolutePath()+"\\desktop";
+		String outputFileName = fileSdf.format(new Date()) +"_" + session.getAttribute("userName") + ".xlsx";
 		Files.createDirectories(new File(outputFilePath).toPath());
 		FileOutputStream stream = new FileOutputStream(outputFilePath + outputFileName);
 		wb.write(stream);
@@ -95,22 +99,24 @@ public class Form2Excel {
 	private void doExcel(DailyReportForm drf) throws IOException {
 		// form情報整え
 		
-		for (int i = 0; i < drf.getDoneThings().size(); i++) {
+		for (int i = 0; i < drf.getDoneThingsList().size(); i++) {
 			// やったことある？と判断
 			// ないだったら削除
-			if (drf.getDoneThings().get(i).getThings()==null||drf.getDoneThings().get(i).getThings().isEmpty()) {
-				drf.getDoneThings().remove(i);
+			if (drf.getDoneThingsList().get(i).getThings()==null||drf.getDoneThingsList().get(i).getThings().isEmpty()) {
+				drf.getDoneThingsList().remove(i);
 				i--;
 			}
 			// 改行
-			drf.getDoneThings().get(i).getThings().replaceAll("(.{15})", "\n");
-			drf.getDoneThings().get(i).getImprovement().replaceAll("(.{20})", "\n");
+			drf.getDoneThingsList().get(i).getThings().replaceAll("(.{15})", "\n");
+			drf.getDoneThingsList().get(i).getImprovement().replaceAll("(.{20})", "\n");
 		}
 		
 		drf.getReflection().replaceAll("(.{40})", "\n");
 		
 		dailyReportForms.setDoneThings(drf.getDoneThings());
 		dailyReportForms.setReflection(drf.getReflection());
+		
+		dailyReportForms.getDoneThingsList().forEach(s->{System.out.println(s.getThings());});
 				
 	}
 	
@@ -120,7 +126,7 @@ public class Form2Excel {
 			ws.createRow(row_pos);
 		}
 		if (ws.getRow(row_pos).getCell(col_pos) == null) {
-			HSSFCell newCell = ws.getRow(row_pos).createCell(col_pos);
+			XSSFCell newCell = ws.getRow(row_pos).createCell(col_pos);
 			try {
 				newCell.setCellStyle(ws.getRow(2).getCell(1).getCellStyle());
 			} catch (Exception ex) {

13ccb96c0597154776a59d24b2ea4353fe159667
wakana-robin <135582700+wakana-robin@users.noreply.github.com>
2023-06-07 15:19:09 +0900

    up

--- GoodSunLMS/src/main/java/com/reality/controller/SelectController.java
+++ GoodSunLMS/src/main/java/com/reality/controller/SelectController.java
@@ -1,7 +1,6 @@
 package com.reality.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,5 +29,10 @@ public class SelectController {
 	@GetMapping("/attendanceSystem")
 	public String attendanceSystem() {
 		return "attendanceSystem";
+	}
+	
+	@GetMapping("/return")
+	public String returnToSelect() {
+		return "select";
 	}
 }

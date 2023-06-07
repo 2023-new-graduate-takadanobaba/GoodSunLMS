a4a81b3436dfb6ff6f32d676d06757a70b982f7e
ou.ikutei <ou.ikutei@GS0112>
2023-06-07 15:25:14 +0900

    comment

--- GoodSunLMS/src/main/java/com/reality/repository/UserRepository.java
+++ GoodSunLMS/src/main/java/com/reality/repository/UserRepository.java
@@ -1,9 +1,9 @@
-package com.reality.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.reality.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
+//package com.reality.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.reality.entity.User;
+//
+//public interface UserRepository extends JpaRepository<User, Integer>{
+//
+//}

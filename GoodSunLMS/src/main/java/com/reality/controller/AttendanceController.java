5ee45bda89e89ec7439907c66653d3832d0a6b79
wakana-robin <135582700+wakana-robin@users.noreply.github.com>
2023-06-09 14:55:31 +0900

    xx

--- GoodSunLMS/src/main/java/com/reality/controller/AttendanceController.java
+++ GoodSunLMS/src/main/java/com/reality/controller/AttendanceController.java
@@ -34,7 +34,6 @@ public class AttendanceController {
 		Attendance attendance = new Attendance();
 		User user = userRepository.findByUserName(session.getAttribute("userName").toString());
 		Date date = new Date();
-//		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		attendance.setDate(date);
 		attendance.setStartTime("9:00");
 		attendance.setEndTime("18:00");

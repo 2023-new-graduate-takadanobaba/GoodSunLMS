24124ed4c7409e62d770b853fa6f5d30d830b06e
wakana-robin <135582700+wakana-robin@users.noreply.github.com>
2023-06-09 15:36:47 +0900

    愚痴相談登録機能編集

--- GoodSunLMS/src/main/java/com/reality/controller/ComplaintsController.java
+++ GoodSunLMS/src/main/java/com/reality/controller/ComplaintsController.java
@@ -1,13 +1,40 @@
 package com.reality.controller;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.reality.entity.Attendance;
+import com.reality.entity.User;
+import com.reality.repository.AttendanceRepository;
+import com.reality.repository.UserRepository;
+
+import jakarta.servlet.http.HttpSession;
 @Controller
 public class ComplaintsController {
-
+	@Autowired
+	AttendanceRepository attendanceRepository;
+	
+	@Autowired
+	UserRepository userRepository;
+	
 	@PostMapping("/complaints")
-	public String complaints() {
+	public String complaints(Model model, HttpSession session, String startTime, String endTime, String place) {
+		Attendance attendance = new Attendance();
+		User user = userRepository.findByUserName(session.getAttribute("userName").toString());
+		Date date = new Date();
+		attendance.setDate(date);
+		attendance.setStartTime(startTime);
+		attendance.setEndTime(endTime);
+		attendance.setWorkHours("0:30");
+		attendance.setPlace(place);
+		attendance.setProject("新人教育（愚痴相談）");
+		attendance.setUser(user);
+		attendanceRepository.save(attendance);
+		model.addAttribute("attendance");
 		return "complaintsMessage";
 	}
 }

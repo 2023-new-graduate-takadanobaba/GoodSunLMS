b8e3c76efc343bdf1a842a7bb6f3042035c17112
ou.ikutei <ikutei.ou@goodsun-hd.com>
2023-07-04 13:22:58 +0900

    send mail cc

--- GoodSunLMS/src/main/resources/static/js/sendMail.js
+++ GoodSunLMS/src/main/resources/static/js/sendMail.js
@@ -22,9 +22,10 @@ $(document).ready(function() {
 
 function send() {
     let sendTo = $("input[name='sendTo']").val();
+    let cc = $("input[name='sendCC']").val();
     let tcont = $("input[name='title']").val();
     let tecont = $('textarea[name="text"]').val().replace(/\n/ig,"%0D%0A");
-    window.location.href = "mailto:"+sendTo+"?subject="+tcont+"&body="+tecont;
+    window.location.href = "mailto:"+sendTo+"?cc="+cc+"&subject="+tcont+"&body="+tecont;
 }
 
 function sleep(milliseconds) {

872443d6be0dc165586c2234aa3efe7fb954cf2b
wakana-robin <135582700+wakana-robin@users.noreply.github.com>
2023-07-05 17:09:42 +0900

    theme added

---
+++ GoodSunLMS/src/main/resources/static/js/themeColor.js
@@ -0,0 +1,9 @@
+function changeStyleSheetDark() {
+    var elem = document.getElementById("PageStyleSheet");
+    elem.href = "./css/layoutThemeDark.css";
+}
+
+function changeStyleSheetGirly() {
+    var elem = document.getElementById("PageStyleSheet");
+    elem.href = "./css/layout.css";
+}

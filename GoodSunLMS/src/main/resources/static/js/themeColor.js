34148e4866b2fd5ed7630f59f4455456f79d26a3
wakana-robin <135582700+wakana-robin@users.noreply.github.com>
2023-07-06 11:49:58 +0900

    theme dusty added

--- GoodSunLMS/src/main/resources/static/js/themeColor.js
+++ GoodSunLMS/src/main/resources/static/js/themeColor.js
@@ -1,9 +1,25 @@
-function changeStyleSheetDark() {
-    var elem = document.getElementById("PageStyleSheet");
-    elem.href = "./css/layoutThemeDark.css";
+function changeStyleSheet(ssPath) {
+	sessionStorage.setItem('ssPath', ssPath);
+	var link = document.getElementById('PageStyleSheet');
+	link.href = ssPath;
 }
 
-function changeStyleSheetGirly() {
-    var elem = document.getElementById("PageStyleSheet");
-    elem.href = "./css/layout.css";
-}
+window.addEventListener('load', function(){
+	var ssPath = sessionStorage.getItem('ssPath');
+	if (ssPath) {
+		var link = document.getElementById('PageStyleSheet');
+		link.href = ssPath;
+	}
+});
+
+
+
+//function changeStyleSheetDark() {
+//    var elem = document.getElementById("PageStyleSheet");
+//    elem.href = "./css/layoutThemeDark.css";
+//}
+//
+//function changeStyleSheetGirly() {
+//    var elem = document.getElementById("PageStyleSheet");
+//    elem.href = "./css/layout.css";
+//}
